33a988acdbdf15d164a7ff0be20f01f0732ad666
ngorellana <91963355+ngorellana@users.noreply.github.com>
2024-07-06 13:18:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log(`Se ha hecho clic en el botón ${button.textContent}`);
+    const page = button.textContent.toLowerCase(); // Convierte el texto del botón a minúsculas
+    switch (page) {
+        case 'usuarios':
+            window.location.href='user';
+            break;
+        case 'productos':
+            window.location.href='productos';
+            break;
+        case 'fronEnd':
+           window.location.href='https://ospostog4js.alwaysdata.net/';
+            break;
+        case 'informes':
+            window.location.href='info';
+            break;
+
+        default:
+        console.error(`No se encontró una URL para la página: ${page}`);
+    }
+  });
+});
